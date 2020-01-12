d184920525a0216c519506caa98ef2152d08bcd3
Michał Bartkowiak <bartmichu@gmail.com>
2020-01-12 20:01:07 +0100

    Add photoresistor example

---
+++ photoresistor/trace-readings/main.js
@@ -0,0 +1,21 @@
+/*
+ * ESP8266 / NodeMCU
+ *
+ * Trace photoresistor readings.
+ *
+ * Notes:
+ * - Uses TC53 IO class pattern.
+ * - ESP8266 has only one analog input so the pin property is unused.
+ */
+
+import Analog from 'builtin/analog';
+
+const photoresistor = new Analog();
+
+function scaleResolution(value, resolution) {
+  return value / (1 << resolution);
+}
+
+System.setInterval(() => {
+  trace(scaleResolution(photoresistor.read(), photoresistor.resolution), '\n');
+}, 200);
