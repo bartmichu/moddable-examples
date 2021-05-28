7731f519aab135018471dad45b5bbdfc6caa386e
Michał Bartkowiak <bartmichu@gmail.com>
2021-05-28 16:30:47 +0200

    Change Google style for ESLint

---
+++ .eslintrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+  'env': {
+    'es6': true,
+  },
+  'extends': [
+    'google',
+  ],
+  'globals': {
+    'Atomics': 'readonly',
+    'SharedArrayBuffer': 'readonly',
+  },
+  'parserOptions': {
+    'ecmaVersion': 2018,
+    'sourceType': 'module',
+  },
+  'rules': {
+  },
+};
