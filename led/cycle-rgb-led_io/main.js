f386da0c05d7dc6bd794b772f04f20e13b5bcc66
Michał Bartkowiak <bartmichu@gmail.com>
2023-06-17 22:30:09 +0200

    Improve RGB LED example

--- led/cycle-rgb-led_io/main.js
+++ led/cycle-rgb-led_io/main.js
@@ -1,18 +1,14 @@
 /*
  * Cycle through different colors on an RGB LED without using PWM.
  *
- * Tested on: ESP8266 (NodeMCU, Moddable One).
+ * Tested on: ESP8266 (NodeMCU, Moddable One), RP2040 (Raspberry Pi Pico W).
  *
  * Notes:
  *   - Using the IO module, which is an experimental implementation of ECMA-419.
- *   - An RGB LED is connected as follows:
- *     Red pin -> GPIO 13
- *     Green pin -> GPIO 12
- *     Blue pin -> GPIO 14
  *
  * Parts list:
- *   - Node MCU ESP8266 V2
- *   - Breadboard
+ *   - Raspberry Pi Pico W
+ *   - Beadboard
  *   - Jumper wires
  *   - Common-cathode RGB LED
  *   - 3x 330 ohm Resistor (Orange, Orange, Brown, Gold)
@@ -20,12 +16,17 @@
 
 import DigitalBank from 'embedded:io/digitalbank';
 
+// pins 6,4,2 on Pico W, pins 13,12,14 on NodeMCU V2
+const redPin = 6;
+const greenPin = 4;
+const bluePin = 2;
+
 const rgbLed = new DigitalBank({
-  pins: (1 << 13) | (1 << 12) | (1 << 14),
+  pins: (1 << redPin) | (1 << greenPin) | (1 << bluePin),
   mode: DigitalBank.Output,
 });
 
-const pinMask = [8192, 4096, 16384];
+const pinMask = [1 << redPin, 1 << greenPin, 1 << bluePin];
 let index = 0;
 
 System.setInterval(() => {
